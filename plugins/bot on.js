e9adea0728154ab3b18b676210edd79a6e35739b
ShunaChan69 <111638506+ShunaChan69@users.noreply.github.com>
2022-08-27 10:01:16 +0700

    Update bot on.js

--- plugins/bot on.js
+++ plugins/bot on.js
@@ -1,9 +1,10 @@
 let handler = async (m) => {
     global.db.data.chats[m.chat].isBanned = false
-    m.reply(' *Uaaaaawh🥱, Maap kak Kanna tadi ketiduran😊* ')
-}
+    m.reply(' *Uaaaaawh🥱, Maap kak FM BOT tadi ketiduran😊* ')
+}
+
 handler.tags = ['main']
-handler.command = /^(kannaon)$/i
+handler.command = /^(fmboton)$/i
 
 handler.admin = true
 

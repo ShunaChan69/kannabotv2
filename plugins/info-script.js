c1489a0fbfe73f98abaada3fa377b6dcbabd013d
ShunaChan69 <111638506+ShunaChan69@users.noreply.github.com>
2022-08-27 11:25:23 +0700

    Add files via upload

---
+++ plugins/info-script.js
@@ -0,0 +1,26 @@
+//Credits Jangan Dihapus
+//Thanks For KannaChan & Papah-Chan
+import fetch from 'node-fetch'
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
+
+let str = `*https://github.com/BochilGaming*`
+let wibu = `https://api.zacros.my.id/randomimg/waifu` 
+let thumb = await(await fetch(wibu)).buffer()
+conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://Instagram.com/eabdalmufid_",
+    mediaType: "VIDEO",
+    description: "https://www.instagram.com/eabdalmufid_", 
+    title: 'FM-BOT MultiDevice',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: sgc
+  }
+  } }) 
+          }
+handler.help = ['source code']
+handler.tags = ['info']
+handler.command =  /^(script|sc)$/i
+handler.premium = true
+
+export default handler
